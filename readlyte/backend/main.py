18ac13aea7b204a84cd00099fc483433e2b493f4
Sumit Kumar <sumitk87549@gmail.com>
2026-05-28 11:32:00 +0530

    .

--- readlyte/backend/main.py
+++ readlyte/backend/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
-import ollama
+    from fastapi import FastAPI
+    import ollama
 
-app = FastAPI()
+    app = FastAPI()
 
-@app.post("/chat")
-async def chat()
+    @app.post("/chat")
+    async def chat():
+        pass
+
+    # 
+# Gemma3:4b -good creative
+# cogito:8b - reasoned it is an AI
+# wizardlm2:7b - reasoned for AI then wrote a beautiful text on itself
+# granite4.1:3b - told something creative instantly
+# gemma3n:e2b - overall best
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#     
